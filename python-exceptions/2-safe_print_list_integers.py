b7a79bae19c2625b4d0765571df50af3c187c42c
FeridAbv <feridabv6577@gmail.com>
2026-04-18 12:54:50 +0000

    task2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
-        value = my_list[i]  
+        value = my_list[i]
         try:
             print("{:d}".format(value), end="")
             count += 1
