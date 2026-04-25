3f3a9bbee494dc1e97322956ac74226d8300967a
FeridAbv <feridabv6577@gmail.com>
2026-04-25 13:38:20 +0000

    SFfs

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -4,8 +4,9 @@ Basic Flask application using Jinja templates.
 """
 
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/')
